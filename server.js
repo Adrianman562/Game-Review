883bcb5ccc1ee1656f6b081322ac3380ec425c81
Paul Cervantes <paulcervantes2244@gmail.com>
2023-09-18 21:20:11 -0700

    Server adjustment

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const express = require('express')
+const express = require("express");
 
+const app = express();
+const port = 3001;
 
-
-const port = 3001
-
-
-
-app.listen(port,{
-    console.log('listening on port 3001')
-})
+app.listen(port, () => {
+  console.log("listening on port 3001");
+});
