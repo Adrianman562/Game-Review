e1b156e9226cd37386dd0ab5fd726cbadc0a8663
FROMERO63 <romerofrank63@gmail.com>
2023-09-25 19:50:26 -0700

    adding helpers, routes for createprofile, javascript, and completed front end createprofile.html

---
+++ public/javascript/createprofile.js
@@ -0,0 +1,47 @@
+const createProfile = document.getElementById('createProfile');
+const submit= document.getAnimations('submitButton');
+
+createProfile
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let email = document.getElementById('emailInput').value;
+    let username = document.getElementById('usernameInput').value.trim();
+    let password = document.getElementById('passwordInput').value.trim();
+    let confirm = document.getElementById('confirmInput').value.trim();
+    let genres = document.querySelectorAll('.checkbox').value;
+    let avatar = document.getElementById('avatarInput').value;
+    let aboutMe = document.getElementById('textArea').value;
+
+
+    // Create object with the new profile info
+    const newProfile = {
+      email,
+      username,
+      password,
+      confirm,
+      genres,
+      avatar,
+      aboutMe,
+    };
+
+    // Fetch POST request to the server
+    fetch('api/profiles', {
+      method: 'POST',
+      body: JSON.stringify(newProfile),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.status);
+        email = '';
+        username = '';
+        password = '';
+        confirm = '';
+        genres = '';
+        avatar = '';
+        aboutMe = '';
+      });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
