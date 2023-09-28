30043333fc20a93687afa2970be1ad4f0ad073cc
FROMERO63 <romerofrank63@gmail.com>
2023-09-28 14:21:50 -0700

    added profile handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,31 +11,36 @@ router.get('/createprofile', async (req, res)=> {
     res.render('createprofile', {title: 'main'});
   });
 
+     //create profile route
+router.get('/profile', async (req, res)=> {
+  res.render('profile', {title: 'main'});
+});
 
-  // GET one profile
-router.get('/profile/:id', async (req, res) => {
-    try {
-      const dbUserData = await Users.findByPk(req.params.id, {
-        include: [
-          {
-            model: Reviews,
-            attributes: [
-              'review_id',
-              'game_title',
-              'rating',
-              'comments',
-            ],
-          },
-        ],
-      });
+
+//   // GET one profile
+// router.get('/profile/:id', async (req, res) => {
+//     try {
+//       const dbUserData = await Users.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Reviews,
+//             attributes: [
+//               'review_id',
+//               'game_title',
+//               'rating',
+//               'comments',
+//             ],
+//           },
+//         ],
+//       });
   
-      const user = dbUserData.get({ plain: true });
-      res.render('profile', { user, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+//       const user = dbUserData.get({ plain: true });
+//       res.render('profile', { user, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   });
 
 
  // Login route
