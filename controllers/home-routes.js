51c656eb2d1fc8d0a3a66a2d933463c312b0df24
Paul Cervantes <paulcervantes2244@gmail.com>
2023-11-22 17:53:39 -0800

    signin redo

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,59 +1,65 @@
-const router = require('express').Router();
-const { Users, Reviews } = require('../models');
+const router = require("express").Router();
+const withAuth = require("../auth/auth");
+const { Users, Reviews } = require("../models");
 
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await Users.findByPk(req.session.user_id, {});
 
-router.get('/', async (req, res)=> {
-  res.render('homepage', {title: 'main'});
+    res.render("profile", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Check home endpoint!" });
+  }
+});
+/*
+// GET one profile
+router.get("/profile/:id", async (req, res) => {
+  try {
+    const dbUserData = await Users.findByPk(req.params.id, {
+      include: [
+        {
+          model: Reviews,
+          attributes: [
+            "review_id",
+            "game_title",
+            "rating",
+            "genre",
+            "comments",
+          ],
+        },
+      ],
+    });
+
+    const user = dbUserData.get({ plain: true });
+    res.render("profile", { user, logged_in: req.session.logged_in });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-   //create profile route
-router.get('/createprofile', async (req, res)=> {
-    res.render('createprofile', {title: 'main'});
-  });
+// GET one review
+router.get("/review/:id", async (req, res) => {
+  try {
+    const dbReviewData = await Reviews.findByPk(req.params.id);
 
-     //create profile route
+    const review = dbReviewData.get({ plain: true });
+    res.render("review", { review, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json({ message: "Check review endpoint!" });
+  }
+});
+*/
+//create profile route
+router.get("/createprofile", async (req, res) => {
+  res.render("createprofile");
+});
 
+// Login route
+router.get("/login", (req, res) => {
+  res.render("homepage");
+});
 
-// router.get('/profile', async (req, res)=> {
-//   res.render('profile', {title: 'main'});
-// });
-
-
-//   // GET one profile
-// router.get('/profile/:id', async (req, res) => {
-//     try {
-//       const dbUserData = await Users.findByPk(req.params.id, {
-//         include: [
-//           {
-//             model: Reviews,
-//             attributes: [
-//               'review_id',
-//               'game_title',
-//               'rating',
-//               'comments',
-//             ],
-//           },
-//         ],
-//       });
-  
-//       const user = dbUserData.get({ plain: true });
-//       res.render('profile', { user, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json(err);
-//     }
-//   });
-
-
- // Login route
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-    res.render('login');
-  });
-  
 module.exports = router;
-
-  
--- models/reviews.js
+++ models/reviews.js
@@ -1,42 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Reviews extends Model {}
 
-Reviews.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  game_title:{
-    type: DataTypes.STRING,
-
-
-  },
-  content:{
-    type:DataTypes.INTEGER,
-    allowNull:false,
-  },
-  rating:{
-    type:DataTypes.INTEGER,
-
-  },
-  user_id:{
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'users',
-      key: 'id',
+Reviews.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    game_title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
     },
   },
-},
-{
-  sequelize,
-  freezeTableName: true,
-  underscored: true,
-  modelName: 'reviews',
-
-
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "reviews",
+  }
+);
 module.exports = Reviews;
-
